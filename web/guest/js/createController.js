31228edabd439757cdd7078e690e30bcd620b184
kdudemaster <kareemelhadidi@gmail.com>
2017-03-03 09:23:53 -0500

    Adding Create Event API Calls

--- web/guest/js/createController.js
+++ web/guest/js/createController.js
@@ -1,5 +1,5 @@
 var myApp = angular.module("myApp", [])   
-   .controller('createCtrl', ['$scope',	
+   .controller('createCtrl', ['$scope',	'$http',
   	function($scope, $http) {
   		self = this;
   		self.check = false;
@@ -7,7 +7,9 @@ var myApp = angular.module("myApp", [])
     $scope.create = function(){
     	var params = "{password:" + eventCode + ", explicit:" + self.check + "}"
     	//window.alert("test:" + self.eventCode);
-    	$http.post(url: 'localhost/CreateEvent', params))
+    	$http
+
+      .post(url: 'localhost/CreateEvent', params)
     	.success(function(data, status, headers, config) {
     		window.location('/index.html');
     		eventID = data.EventId; 
