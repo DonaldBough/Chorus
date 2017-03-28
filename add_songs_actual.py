be9bc47be3894d096420eebb6f51fa026ba608ae
chan135 <chan135@purdue.edu>
2017-03-28 18:26:16 -0400

    Update add_songs_actual.py

--- add_songs_actual.py
+++ add_songs_actual.py
@@ -3,13 +3,9 @@ import sys
 import spotipy
 import spotipy.util as util
 #username needs to be obtained from the database
-#username = '1210281728'
-#username = '1235536440'
-
 
 #track_ids should be a variable passed into the function
 
-#track_ids = ["2gFvRmQiWg9fN9i74Q0aiw", "4Km5HrUvYTaSUfiSGPJeQR", "7BKLCZ1jbUBVqRi2FVlTVw", "3bi8yEuK44vLcbjHkPH0u1", "5SDVX9gpSXoE0M6KZt4EBF", "0O6jl8Zamz6TGF0nUwMQsF", "4RnfMhMUMqHlrn4V6A3KfS", "6F5c58TMEs1byxUstkzVeM", "3cfOd4CMv2snFaKAnMdnvK"]
 #scope = 'playlist-read-private playlist-read-collaborative playlist-modify-private'
 #token = util.prompt_for_user_token(username,scope,client_id='3c6df9a90b934200856b352829f09fd0',client_secret='694b8ac2f8cb478796b304fd6f1fd082',redirect_uri='http://localhost/')
 
