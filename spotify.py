99a3f84b68826444ff78aef8585ce983e1300960
tpoozhi <tpoozhi@purdue.edu>
2017-03-31 15:12:09 -0400

    updated spotipy in server

--- spotify.py
+++ spotify.py
@@ -18,9 +18,10 @@ class Spotify:
     def createPlaylist():
         #refresh_access_token(
         # Creates a playlist for a user
-        username = '1210281728'
-        #username =  '1235536440'
-        playlist_name = 'Chorus3'
+        #username = '1210281728'
+        username =  '1237666383' 
+        #tim user id
+        playlist_name = 'Chorus'
         scope = 'playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify'
 
         #put server url for redirect url
@@ -36,11 +37,11 @@ class Spotify:
             print("Can't get token for", username)
             return 0;
 
-    def addSongs(token):
-        username = '1210281728'
+    def addSongs(token, trackID):
+        username =  '1237666383' 
         #username = '1235536440'
 
-        track_ids = ["2gFvRmQiWg9fN9i74Q0aiw", "4Km5HrUvYTaSUfiSGPJeQR", "7BKLCZ1jbUBVqRi2FVlTVw", "3bi8yEuK44vLcbjHkPH0u1", "5SDVX9gpSXoE0M6KZt4EBF", "0O6jl8Zamz6TGF0nUwMQsF", "4RnfMhMUMqHlrn4V6A3KfS", "6F5c58TMEs1byxUstkzVeM", "3cfOd4CMv2snFaKAnMdnvK"]
+        #track_ids = ["2gFvRmQiWg9fN9i74Q0aiw", "4Km5HrUvYTaSUfiSGPJeQR", "7BKLCZ1jbUBVqRi2FVlTVw", "3bi8yEuK44vLcbjHkPH0u1", "5SDVX9gpSXoE0M6KZt4EBF", "0O6jl8Zamz6TGF0nUwMQsF", "4RnfMhMUMqHlrn4V6A3KfS", "6F5c58TMEs1byxUstkzVeM", "3cfOd4CMv2snFaKAnMdnvK"]
         #scope = 'playlist-read-private playlist-read-collaborative playlist-modify-private'
         #token = util.prompt_for_user_token(username,scope,client_id='3c6df9a90b934200856b352829f09fd0',client_secret='694b8ac2f8cb478796b304fd6f1fd082',redirect_uri='http://localhost/')
         sp = spotipy.Spotify(auth=token)
@@ -51,11 +52,11 @@ class Spotify:
 
         # get the first album uri
         for playlist in playlists['items']:
-            if(playlist['name'] == "Chorus3"):
+            if(playlist['name'] == "Chorus"):
                 playlist_id = playlist['id']
 
         #playlist_id = '59E9xmQUzDY7H9yKNlj48F'
         print track_ids
-        sp.user_playlist_add_tracks(username, playlist_id, track_ids)
+        sp.user_playlist_add_tracks(username, playlist_id, trackID)
         print("playlist id: " + playlist_id)
         return playlist_id
